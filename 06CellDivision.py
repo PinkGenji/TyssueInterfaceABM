d45f15dfe94989dc7ca3e95f4b700923cc0c7f20
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-05-15 15:12:56 +1200

    update 06CellDivision.py

--- 06CellDivision.py
+++ 06CellDivision.py
@@ -65,47 +65,72 @@ fig, ax = sheet_view(sheet, **draw_specs)
 fig.set_size_inches(12, 5)
 
 
+from tyssue.io.hdf5 import save_datasets, load_datasets
+# redefine cell_division from monolayer related topology module
+from tyssue.topology.monolayer_topology import cell_division
+
+from tyssue.core.monolayer import Monolayer
+from tyssue.geometry.bulk_geometry import ClosedMonolayerGeometry as monolayer_geom
+from tyssue.dynamics.bulk_model import ClosedMonolayerModel
+from tyssue.draw import highlight_cells
+
+wd = r"C:\Users\lyu195\Documents\GitHub\tyssueHello"
+os.chdir(wd)
+
+os.path.isfile('small_ellipsoid.hf5')  #Check working directory set correctly.
+
+h5store = 'small_ellipsoid.hf5'
+
+datasets = load_datasets(h5store, data_names=['vert', 'edge', 'face', 'cell'])
+
+specs = config.geometry.bulk_spec()
+monolayer = Monolayer('ell', datasets, specs)
+monolayer_geom.update_all(monolayer)
+
+specs = {
+    "edge": {
+        "line_tension": 0.0,
+    },
+    "face": {
+        "contractility": 0.01,
+    },
+    "cell": {
+        "prefered_vol": monolayer.cell_df['vol'].median(),
+        "vol_elasticity": 0.1,
+        "prefered_area": monolayer.cell_df['area'].median(),
+        "area_elasticity": 0.1,
+    },
+    "settings": {
+        'lumen_prefered_vol': monolayer.settings['lumen_vol'],
+        'lumen_vol_elasticity': 1e-2
+
+    }
+}
+
+monolayer.update_specs(specs, reset=True)
+
+res = solver.find_energy_min(monolayer, monolayer_geom, ClosedMonolayerModel)
+
+mother = 8
+daughter = cell_division(monolayer, mother, 
+                         orientation='vertical')
+
+monolayer.validate()
+
 # =============================================================================
-# Since the 3D drawing of Tyssue is not well written, we shall skip 3D part!
+#
+# We shall skip the following 3D drawing, since tyssue is not very well written
+# for 3D drawing.
+#
+# rho = np.linalg.norm(monolayer.vert_df[monolayer.coords], axis=1)
+# draw_specs['edge']['color'] = rho
+# draw_specs['face']['visible'] = True
+# 
+# ipv.clear()
+# highlight_cells(monolayer, mother, reset_visible=True)
+# fig, mesh = sheet_view(monolayer, mode="3D",coords=['z', 'x', 'y'], **draw_specs)
+# fig
 # =============================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
