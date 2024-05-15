2fb572d156d615381c678e2f3e3a5bbc52dc95c1
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-05-15 16:03:26 +1200

    create 07EventManager.py

---
+++ 07EventManager.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+This file is for learning 07EventManager of the tyssue package.
+"""
+
+'''
+The event manager in tyssue is a system that manage the different behaviour of
+a simulation.
+
+It is compsoed of two lists:
+    current: list of actions to be executed during the current simulation step.
+    next: list of actions to be executed for the next simulation step, this list
+    is fill during the current simulation step.
+
+A behaviour is a function which describes/contains the actions requested to 
+realize an event. 
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+This is the end of the file.
+'''
