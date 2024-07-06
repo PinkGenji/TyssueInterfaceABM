b171d1db1f842ad4e0490dbab0439c0aac0313a7
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-07-07 10:36:25 +1200

    create code to investigate basics

---
+++ Investigate relationships in basics.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Let us investigate the relationships between different data structures
+that deals with different components of the model (edge, face, vertices).
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+This is the end of the script.
+'''
