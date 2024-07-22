4e3eb4a62b0ad71ec62c99f2090c47cc905ee6d6
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-07-22 19:00:47 +1200

    backup

---
+++ Hello Tyssue learning/three cell problem.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+This script uses a three cell system as a demo.
+"""
+
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+
+from tyssue.generation import three_faces_sheet
+
+
+""" start the project """
+# Generate the cell sheet as three cells.
+sheet = Sheet.planar_sheet_2d('face', nx = 3, ny=4, distx=2, disty=2)
+sheet.sanitize(trim_borders=True)
+geom.update_all(sheet)
+
+nondim_specs = config.dynamics.quasistatic_plane_spec()
+sheet.update_specs(nondim_specs, reset = True)
+solver = QSSolver()
+res = solver.find_energy_min(sheet, geom, smodel)
+
+
+# Draw the cell mesh with face labelling.
+fig, ax= sheet_view(sheet)
+
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+
+daughter = cell_division(sheet, 1, geom, angle= np.pi)
+geom.update_all(sheet)
+
+
+#show index
+print(sheet.face_df['area'])
+
+
+"""
+This is the end of the script.
+"""
