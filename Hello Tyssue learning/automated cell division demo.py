930d5d121a42396ccac16b4be8bd4a8564fba05f
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-08-01 09:54:38 +1200

    created the demo file.

---
+++ Hello Tyssue learning/automated cell division demo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+This script is to run a more automated simulation of cell division.
+This simulation aims to provide better understanding on the data frame changes
+during the process.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+This is the end of the script.
+"""
