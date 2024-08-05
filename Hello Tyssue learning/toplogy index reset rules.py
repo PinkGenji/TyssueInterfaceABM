a613b423418983543edcc9cb064011503637a29e
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-08-05 15:14:38 +1200

    update topology index reset rules.

--- Hello Tyssue learning/toplogy index reset rules.py
+++ Hello Tyssue learning/toplogy index reset rules.py
@@ -4,12 +4,76 @@ This script is to investigate the how the built-in function reset_all() works.
 This function is important as it resets the indexing of the vertex model.
 """
 
+# -*- coding: utf-8 -*-
+"""
+This script runs a simulation of cell divisions with 4 cells initially.
+"""
+
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division
+
+# Event manager
+from tyssue.behaviors import EventManager
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
 
 
+# Generate the cell sheet as three cells.
+sheet = Sheet.planar_sheet_2d('face', nx = 3, ny=4, distx=2, disty=2)
+sheet.sanitize(trim_borders=True)
 
+#reset index.
+sheet.reset_index(order=True)
+geom.update_all(sheet)
 
+# view with face and vertex labelling.
+fig, ax = sheet_view(sheet)
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
 
+fig, ax= sheet_view(sheet)
+for vert, data in sheet.vert_df.iterrows():
+    ax.text(data.x, data.y+0.1, vert)
 
+""" 
+When order = True, the index, the edges are resorted such that for each face, 
+the index of vertices are labelled in clockwise.
+
+Set order = True also removes the "empty faces".
+
+ """
 
 
 
