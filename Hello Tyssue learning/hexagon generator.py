f41fecc1a89a802575a45ca80e603d281bfdd581
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-09-09 17:06:32 +1200

    updated hexagon generator.py
    Defined the face_deleting function.

--- Hello Tyssue learning/hexagon generator.py
+++ Hello Tyssue learning/hexagon generator.py
@@ -24,10 +24,9 @@ from tyssue import Sheet, config #import core object
 from tyssue import PlanarGeometry as geom #for simple 2d geometry
 
 # For cell topology/configuration
-from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.sheet_topology import type1_transition, cell_division
 from tyssue.topology.base_topology import collapse_edge, remove_face, merge_border_edges
 from tyssue.topology.sheet_topology import split_vert as sheet_split
-from tyssue.topology.bulk_topology import split_vert as bulk_split
 from tyssue.topology import condition_4i, condition_4ii
 
 
@@ -39,35 +38,97 @@ from tyssue.draw import sheet_view, highlight_cells
 #I/O
 from tyssue.io import hdf5
 
-
-# Generate a bilayer structure.
-bilayer = Sheet.planar_sheet_2d(identifier='bilayer', nx = 10, ny = 4, distx = 1, disty = 1)
-
-geom.update_all(bilayer)
-
-# Have a look of the generated bilayer.
-sheet_view(bilayer)
-fig, ax = sheet_view(bilayer)
-for face, data in bilayer.face_df.iterrows():
-    ax.text(data.x, data.y, face)
-
+# Draw voronoi diagram
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from tyssue.generation import hexa_grid2d, from_2d_voronoi
 
-nx = 10
+nx = 4
 ny=4
-distx=5
-disty = 2
+distx=1
+disty = 1
 noise = 0
 
 grid = hexa_grid2d(nx, ny, distx, disty, noise)
 datasets = from_2d_voronoi(Voronoi(grid))
 
 vor = Voronoi(grid)
-import matplotlib.pyplot as plt
+
 fig = voronoi_plot_2d(vor)
 plt.show()
 
-vor.ridge_point
 
 
+""" try: with trium, then remove the nonhexagons. """
+bilayer=Sheet.planar_sheet_2d(identifier='bilayer', nx = 4, ny = 6, distx = 1, disty = 1)
+bilayer.sanitize(trim_borders=True)
+geom.update_all(bilayer)
+
+# Have a look of the generated bilayer.
+fig, ax = sheet_view(bilayer)
+for face, data in bilayer.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+cleaned = remove_face(bilayer, 0)
+geom.update_all(bilayer)
+
+fig, ax = sheet_view(bilayer)
+for face, data in bilayer.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+
+""" Try: without using trim, just delete the outside faces """
+bilayer=Sheet.planar_sheet_2d(identifier='bilayer', nx = 4, ny = 4, distx = 1, disty = 1)
+geom.update_all(bilayer)
+
+# Have a look of the generated bilayer.
+fig, ax = sheet_view(bilayer, edge = {'head_width':0.1})
+for face, data in bilayer.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+# Find the outside-edges and faces associated with them, and get rid off them.
+invalid_edge = bilayer.get_invalid()   
+bilayer.remove(invalid_edge)  
+
+fig, ax = sheet_view(bilayer, edge = {'head_width':0.1})
+for face, data in bilayer.face_df.iterrows():
+    ax.text(data.x, data.y, face) 
+
+
+
+def delete_face(sheet_obj, face_deleting):
+    """
+
+    
+    Parameters
+    ----------
+    sheet_obj : Epithelium
+        An Epithelium 'Sheet' object from Tyssue.
+    face_deleting : Int
+        The index of the face to be deleted.
+
+    Returns
+    -------
+    A Pandas Data Frame that deletes the face, with border edges are single
+    arrowed, without index resetting.
+
+    """
+    # Compute all edges associated with the face, then drop these edges in df.
+    associated_edges = sheet_obj.edge_df[sheet_obj.edge_df['face'] == face_deleting]
+    sheet_obj.edge_df.drop(associated_edges.index, inplace = True)
+    
+    # All associated edges are removed, now remove the 'empty' face and reindex.
+    sheet_obj.face_df.drop(face_deleting , inplace =True)
+
+
+
+delete_face(bilayer, 3)
+delete_face(bilayer,2)
+# Plot the figure to check.
+fig, ax = sheet_view(bilayer, edge = {'head_width':0.1})
+for face, data in bilayer.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+
+
+
+""" This is the end of the script """
