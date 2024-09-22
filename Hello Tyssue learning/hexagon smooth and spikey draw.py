e4f74d2faa135a0e36ded26054101a5fd864ffa3
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-09-23 10:56:37 +1200

    update spikey smooth draw
    obtained code for both type of top boundary

--- Hello Tyssue learning/hexagon smooth and spikey draw.py
+++ Hello Tyssue learning/hexagon smooth and spikey draw.py
@@ -11,8 +11,8 @@ from tyssue.generation import  from_2d_voronoi
 
 
 """ Code for spikey top """
-nx = 3
-ny=2
+nx = 5
+ny=5
 distx=1
 disty = 1
 noise = 0
@@ -20,10 +20,10 @@ noise = 0
 def hexa_grid(nx, ny, distx, disty, noise=None):
     """Creates an hexagonal grid of points
     """
-    cy, cx = np.mgrid[0:ny+2, 0:nx+2]
+    cy, cx = np.mgrid[0:ny, 0:nx]
     cx = cx.astype(float)
     cy = cy.astype(float)
-    cy[::2, :] += 0.5
+    cx[::2, :] += 0.5
 
     centers = np.vstack([cx.flatten(), cy.flatten()]).astype(float).T
     centers[:, 0] *= distx
@@ -31,8 +31,6 @@ def hexa_grid(nx, ny, distx, disty, noise=None):
     if noise is not None:
         pos_noise = np.random.normal(scale=noise, size=centers.shape)
         centers += pos_noise
-# uncomment the following line to get a voronoi diagram with flat top/bot.
-    centers = np.flip(centers,1)
     return centers
 
 
@@ -47,5 +45,32 @@ plt.show()
 
 """ Code for smooth top. """
 
+def hexa_grid(nx, ny, distx, disty, noise=None):
+    """Creates an hexagonal grid of points
+    """
+    cy, cx = np.mgrid[0:ny, 0:nx]
+    cx = cx.astype(float)
+    cy = cy.astype(float)
+    cx[::2, :] += 0.5
 
+    centers = np.vstack([cx.flatten(), cy.flatten()]).astype(float).T
+    centers[:, 0] *= distx
+    centers[:, 1] *= disty
+    if noise is not None:
+        pos_noise = np.random.normal(scale=noise, size=centers.shape)
+        centers += pos_noise
+
+    # Flip the x and y to get smooth top
+    centers = np.flip(centers,1)
+    return centers
+
+
+grid = hexa_grid(nx, ny, distx, disty, noise)
+#grid = np.flip(grid,1)
+datasets = from_2d_voronoi(Voronoi(grid))
+
+vor = Voronoi(grid)
+
+fig = voronoi_plot_2d(vor)
+plt.show()
 
