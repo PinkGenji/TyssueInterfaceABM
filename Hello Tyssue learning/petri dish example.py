036f952ac403c714cfdc5bad6c9642d83406fce5
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-10-02 12:11:42 +1300

    created petri dish example
    This script will serve as the starting point to explain vertex modelling

---
+++ Hello Tyssue learning/petri dish example.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+This script contains an example of vertex modelling for petri dish like tissue 
+cells.
+"""
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division
+
+# Event manager
+from tyssue.behaviors import EventManager
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+
+from my_headers import delete_face, xprod_2d, put_vert
+
+
+""" start the project """
+# Generate the cell sheet as three cells.
+num_x = 9
+num_y = 8
+
+sheet = Sheet.planar_sheet_2d('face', nx = num_x, ny=num_y, distx=2, disty=2)
+
+geom.update_all(sheet)
+
+# remove non-enclosed faces
+sheet.remove(sheet.get_invalid())
+
+# Plot the figure to see the index.
+fig, ax = sheet_view(sheet)
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+    
+for i in list(range(1,num_y*(num_x+1))):
+    delete_face(sheet, i*num_x)
+    delete_face(sheet, i*(num_x+1))
+sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
+
+# Plot figures to check.
+# Draw the cell mesh with face labelling and edge arrows.
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+
+
+"""
+This is the end of the script.
+"""
