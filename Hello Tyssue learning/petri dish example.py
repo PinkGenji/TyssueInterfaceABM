06b741fc16caf8961e5e97ca04c1eaa9492578e8
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-10-03 08:56:36 +1300

    update petri dish example

--- Hello Tyssue learning/petri dish example.py
+++ Hello Tyssue learning/petri dish example.py
@@ -46,7 +46,7 @@ from tyssue.draw import sheet_view, highlight_cells
 from my_headers import delete_face, xprod_2d, put_vert
 
 
-""" start the project """
+""" start the project. """
 # Generate the cell sheet as three cells.
 num_x = 9
 num_y = 8
@@ -63,9 +63,9 @@ fig, ax = sheet_view(sheet)
 for face, data in sheet.face_df.iterrows():
     ax.text(data.x, data.y, face)
     
-for i in list(range(1,num_y*(num_x+1))):
-    delete_face(sheet, i*num_x)
-    delete_face(sheet, i*(num_x+1))
+for i in list(range(num_x, num_y*(num_x+1), 2*(num_x+1) )):
+    delete_face(sheet, i)
+    delete_face(sheet, i+1)
 sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
 
 # Plot figures to check.
@@ -73,6 +73,109 @@ sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwis
 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
 for face, data in sheet.face_df.iterrows():
     ax.text(data.x, data.y, face)
+    
+# add mechanical properties.
+specs = {
+    'edge': {
+        'is_active': 1,
+        'line_tension': 0.12,
+        'ux': 0.0,
+        'uy': 0.0,
+        'uz': 0.0
+    },
+   'face': {
+       'area_elasticity': 1.0,
+       'contractility': 0.04,
+       'is_alive': 1,
+       'prefered_area': 1.0},
+   'settings': {
+       'grad_norm_factor': 1.0,
+       'nrj_norm_factor': 1.0
+   },
+   'vert': {
+       'is_active': 1
+   }
+}
+sheet.update_specs(specs, reset = True)
+geom.update_all(sheet)
+
+# Minimize the potential engery
+solver = QSSolver()
+res = solver.find_energy_min(sheet, geom, smodel)
+
+# Visualize the sheet.
+fig, ax = sheet_view(sheet,  mode = '2D')
+
+""" Grow first, then cells divide. """
+
+# Write behavior function for division_1.
+def division_1(sheet, manager, cell_id, crit_area, growth_rate=0.7, dt=1):
+    """The cells keep growing, when the area exceeds a critical area, then
+    the cell divides.
+    
+    Parameters
+    ----------
+    sheet: a :class:`Sheet` object
+    cell_id: int
+        the index of the dividing cell
+    crit_area: float
+        the area at which 
+    growth_rate: float
+        increase in the area per unit time
+        A_0(t + dt) = A0(t) * (1 + growth_rate * dt)
+    """
+
+    # if the cell area is larger than the crit_area, we let the cell divide.
+    if sheet.face_df.loc[cell_id, "area"] > crit_area:
+        # restore prefered_area
+        sheet.face_df.loc[cell_id, "prefered_area"] = 1.0
+        # Do division
+        daughter = cell_division(sheet, cell_id, geom)
+        # update geometry
+        geom.update_all(sheet)
+        print(f"cell num: {daughter} is born")
+    # if the cell area is less than the threshold, update the area by growth.
+    else:
+        sheet.face_df.loc[cell_id, "prefered_area"] *= (1 + dt * growth_rate)
+
+
+# Initialisation of manager 
+manager = EventManager("face")
+
+from tyssue import History
+
+t = 0
+stop = 3
+
+# The History object records all the time steps 
+history = History(sheet)
+
+while manager.current and t <= stop:
+    for i in sheet.face_df.index:
+        print(f'we are at time step {t}, cell {i} is being checked.')
+        manager.append(division_1, cell_id=i, crit_area=2)
+    # Execute the event in the current list
+        manager.execute(sheet)
+        res = solver.find_energy_min(sheet, geom, smodel)
+    
+    # Find energy min
+    #res = solver.find_energy_min(bilayer, geom, smodel)
+        history.record()
+
+    # Switch event list from the next list to the current list
+        manager.update()
+    fig, ax = sheet_view(sheet, mode="2D")
+    ax.title.set_text(f'Snapshot at t = {t}')
+        
+    t += 1
+
+
+
+
+
+""" Divide first, then daughter cells expand. """
+
+
 
 
 
