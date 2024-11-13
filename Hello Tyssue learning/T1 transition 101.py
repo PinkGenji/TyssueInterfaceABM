c9a6ac8b90f535c8b5774349b2ed699f60ee9d37
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-11-14 04:41:13 +1300

    created T1 transition 101 file

---
+++ Hello Tyssue learning/T1 transition 101.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+This code file contains the primary design of my own T1 transition during my PYR.
+"""
+
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face, add_vert
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division, face_division
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+
+# import my own functions
+from my_headers import delete_face, xprod_2d, put_vert, lateral_split, divisibility_check
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" This is the end of the file. """
