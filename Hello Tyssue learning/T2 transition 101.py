e38c799116ae502f3dd55036ff4c037cbb409fdc
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-11-14 08:04:46 +1300

    update T2 transition 101

--- Hello Tyssue learning/T2 transition 101.py
+++ Hello Tyssue learning/T2 transition 101.py
@@ -1,7 +1,112 @@
 # -*- coding: utf-8 -*-
 """
 This code file contains the primary design of my own T2 transition
+
+The logic of T2 transition is that, if a triangle is formed, and the triangle
+has small area, then we need to remove it by merging the triangle into a single
+vertex that has the coordinate of the centroid of the triangle, where centroid
+is an interior point that have equal distance from to all triangle vertices.
+
 """
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face, add_vert
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division, face_division
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+
+# import my own functions
+from my_headers import delete_face, xprod_2d, put_vert, lateral_split, divisibility_check
+
+
+# =============================================================================
+# Start the working.
+# =============================================================================
+# Generate the cell sheet as three cells.
+num_x = 4
+num_y = 4
+
+sheet = Sheet.planar_sheet_2d('face', nx = num_x, ny=num_y, distx=2, disty=2)
+
+geom.update_all(sheet)
+
+# remove non-enclosed faces
+sheet.remove(sheet.get_invalid())
+
+# Plot the figure to see the index.
+fig, ax = sheet_view(sheet)
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+    
+for i in list(range(num_x, num_y*(num_x+1), 2*(num_x+1) )):
+    delete_face(sheet, i)
+    delete_face(sheet, i+1)
+sheet.get_extra_indices()
+sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
+
+# Visualize the sheet.
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.vert_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+# Visualize the sheet.
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+# Find the centroid location of cell 6
+for i in sheet.edge_df.index:
+    if sheet.edge_df.loc[i, 'face'] == 6:
+        centroid = sheet.edge_df.loc[i,['fx', 'fy']]
+        print(f'the centroid is {centroid}')
+        
+        
+remove_face(sheet, 6)
+geom.update_all(sheet)
+sheet_view(sheet)
+# Visualize the sheet.
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.vert_df.iterrows():
+    ax.text(data.x, data.y, face)
+
+sheet.vert_df.loc[42]
+
+
+
+
+
+
+
 
 
 
