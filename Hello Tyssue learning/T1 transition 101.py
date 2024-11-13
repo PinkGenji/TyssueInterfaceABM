e0f2a84067fcd4c5d0d2654349d89f625be543ad
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-11-14 06:17:02 +1300

    finished T1 transition 101

--- Hello Tyssue learning/T1 transition 101.py
+++ Hello Tyssue learning/T1 transition 101.py
@@ -43,15 +43,57 @@ from tyssue.draw import sheet_view, highlight_cells
 # import my own functions
 from my_headers import delete_face, xprod_2d, put_vert, lateral_split, divisibility_check
 
+# =============================================================================
+# Start the working.
+# =============================================================================
+# Generate the cell sheet as three cells.
+num_x = 4
+num_y = 4
 
+sheet = Sheet.planar_sheet_2d('face', nx = num_x, ny=num_y, distx=2, disty=2)
 
+geom.update_all(sheet)
 
+# remove non-enclosed faces
+sheet.remove(sheet.get_invalid())
 
+# Plot the figure to see the index.
+fig, ax = sheet_view(sheet)
+for face, data in sheet.face_df.iterrows():
+    ax.text(data.x, data.y, face)
+    
+for i in list(range(num_x, num_y*(num_x+1), 2*(num_x+1) )):
+    delete_face(sheet, i)
+    delete_face(sheet, i+1)
+sheet.get_extra_indices()
+sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
 
+# Visualize the sheet.
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.vert_df.iterrows():
+    ax.text(data.x, data.y, face)
+    
 
+for index, series in sheet.edge_df.iterrows():
+    if series['srce'] == 14 and series['trgt'] == 45:
+        print(f'The index is: {index}')
 
+sheet.vert_df.loc[45,'y'] +=0.5
+geom.update_all(sheet)
+sheet_view(sheet)
+print('The length of the shortest edge is: '+ str(sheet.edge_df.loc[:,'length'].min()))
 
+# Store the single edges as a list.
+single_edges = sheet.edge_df.index.intersection(sheet.sgle_edges)
 
+for index in single_edges:
+    if sheet.edge_df.loc[index,'length'] < 1.05:
+        print(f'Edge {index} is shorter than threshold')
+        type1_transition(sheet, index, remove_tri_faces=False, multiplier=1.5)
+    else:
+        continue
+geom.update_all(sheet)
+sheet_view(sheet)
 
 
 
