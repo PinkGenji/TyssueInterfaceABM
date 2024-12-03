35733ea7ee3dab36e9ca0fc47138356b5aeef113
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-12-04 11:00:02 +1300

    before delete print contractility vector

--- Hello Tyssue learning/isolated cell.py
+++ Hello Tyssue learning/isolated cell.py
@@ -39,7 +39,7 @@ from tyssue.topology.sheet_topology import remove_face, cell_division, face_divi
 
 # 2D plotting
 from tyssue.draw import sheet_view, highlight_cells
-from tyssue.draw.plt_draw import plot_forces
+from tyssue.draw.plt_draw import plot_forces, plot_forces2
 from tyssue.config.draw import sheet_spec
 # import my own functions
 from my_headers import *
@@ -72,7 +72,7 @@ specs = {
     },
    'face': {
        'area_elasticity': 110,
-       'contractility': 0,
+       'contractility': 0.5,
        'is_alive': 1,
        'prefered_area': 1},
    'settings': {
@@ -96,23 +96,30 @@ for i in sheet.edge_df.index:
         continue
 geom.update_all(sheet)
 
-fig, ax = plot_forces(sheet, geom, smodel, ['x', 'y'], scaling=0.1)
-
-sheet_view(sheet)
-sheet.face_df.loc[:,'area']
-
-
 # We need set the all the threshold value first.
 t1_threshold = 0.1
 t2_threshold = 0.1
 max_movement = t1_threshold/2
 
+move = time_step_bot(sheet, dt=0.01, max_dist_allowed = max_movement )[1]
+
+fig, ax = plot_forces2(sheet, geom, smodel, movement=move, coords=['x', 'y'], dt=0.01, scaling=0.1)
+
+sheet_view(sheet)
+sheet.face_df.loc[:,'area']
+
+
 # Now assume we want to go from t = 0 to t= 0.2, dt = 0.1
 t0 = 0
-t_end = 3
+t_end = 4
 dt = 0.01
 time_points = np.linspace(t0, t_end, int((t_end - t0) / dt) + 1)
 
+
+
+from tyssue.dynamics.base_gradients import length_grad
+from tyssue.dynamics.planar_gradients import area_grad
+area_grad(sheet)
 
 for t in time_points:
     #print(f'start at t= {round(t, 5)}.')
@@ -126,10 +133,38 @@ for t in time_points:
      # Save the new positions back to `vert_df`
     sheet.vert_df.loc[valid_active_verts , sheet.coords] = new_pos
     geom.update_all(sheet)
+plot_forces2(sheet, geom, smodel, movement=movement, coords=['x', 'y'], dt=0.01, scaling=0.1)
 
 sheet_view(sheet)
 area = sheet.face_df.loc[:,'area'][0]
 print(f'After evolve {t} time, area is: {area}')
+
+def to_nd(df, ndim):
+    """
+    Give a new shape to an input data by duplicating its column.
+
+    Parameters
+    ----------
+
+    df : input data that will be reshape
+    ndim : dimension of the new reshape data.
+
+    Returns
+    -------
+
+    df_nd : return array reshaped in ndim.
+
+    """
+    df_nd = np.asarray(df).reshape((df.size, 1))
+    return df_nd
+
+gamma_ = sheet.face_df.eval("contractility * perimeter * is_alive")
+gamma = sheet.upcast_face(gamma_)
+
+grad_srce = -sheet.edge_df[sheet.ucoords] * to_nd(gamma, len(sheet.coords))
+grad_srce.columns = ["g" + u for u in sheet.coords]
+grad_trgt = -grad_srce
+print(grad_srce, grad_trgt)
 
 """
 This is the end of the script.
