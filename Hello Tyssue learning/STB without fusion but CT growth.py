ab3b8e49fd5cb8891c4a812fc54b0d296d5808fb
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-12-10 13:39:37 +1300

    create STB without fusion.py
    This script aims to model how STB changes its shape without fusion but only the growth of CTs

---
+++ Hello Tyssue learning/STB without fusion but CT growth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script describes the movement of STB without fusion, but only the growth
+of CTs underneath it.
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+This is the end of the script.
+"""
