e97bb0dcc6d0327e52c93107bc97bef70a04c900
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-12-10 23:26:25 +1300

    backup on the STB without fusion

--- Hello Tyssue learning/STB without fusion but CT growth.py
+++ Hello Tyssue learning/STB without fusion but CT growth.py
@@ -6,6 +6,229 @@ of CTs underneath it.
 """
 
 
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# For cell topology/configuration
+from tyssue.topology.sheet_topology import type1_transition
+from tyssue.topology.base_topology import collapse_edge, remove_face, add_vert, get_num_common_edges
+from tyssue.topology.sheet_topology import split_vert as sheet_split
+from tyssue.topology.bulk_topology import split_vert as bulk_split
+from tyssue.topology import condition_4i, condition_4ii
+
+## model and solver
+from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
+from tyssue.solvers.quasistatic import QSSolver
+from tyssue.generation import extrude
+from tyssue.dynamics import model_factory, effectors
+from tyssue.topology.sheet_topology import remove_face, cell_division, face_division
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+from tyssue.draw.plt_draw import plot_forces, plot_forces2
+from tyssue.config.draw import sheet_spec
+# import my own functions
+from my_headers import *
+
+rng = np.random.default_rng(70)
+
+# Generate the cell sheet as three cells.
+num_x = 20
+num_y = 2
+sheet = Sheet.planar_sheet_2d('face', nx = num_x, ny=num_y, distx=0.5, disty=0.5)
+geom.update_all(sheet)
+# remove non-enclosed faces
+sheet.remove(sheet.get_invalid())  
+delete_face(sheet, face_deleting = 20)
+delete_face(sheet, face_deleting = 21)
+sheet.reset_index(order=True) 
+geom.update_all(sheet)
+  
+fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
+for face, data in sheet.vert_df.iterrows():
+    ax.text(data.x, data.y, face)
+sheet.get_extra_indices()
+
+
+# First we assign cell type in face data frame.
+num_ct = num_x
+sheet.face_df.loc[0:num_ct-1, "cell_type"] = 'CT'
+sheet.face_df.loc[num_ct:, "cell_type"] = 'ST'
+
+# Then update the edge data frame via 'face' column.
+
+sheet.edge_df['cell_type'] = 'to be set'
+
+for i in list(range(len(sheet.edge_df))):
+    sheet.edge_df.loc[i, 'cell_type'] = sheet.face_df.loc[sheet.edge_df.loc[i,'face'],'cell_type']
+
+sheet.edge_df['cell_type'] = 'to be set'
+
+for i in list(range(len(sheet.edge_df))):
+    sheet.edge_df.loc[i, 'cell_type'] = sheet.face_df.loc[sheet.edge_df.loc[i,'face'],'cell_type']
+
+
+sheet.face_df['division_status'] = 'ready'
+
+# Change the division_status for ST cells.
+for i in list(range(len(sheet.face_df))):
+    if sheet.face_df.loc[i, 'cell_type'] == 'ST':
+        sheet.face_df.loc[i, 'division_status'] = 'N/A'
+    else:
+        continue
+
+""" Add another column to store the growth speed. """
+sheet.face_df['growth_speed'] = 'N/A'
+
+''' Energy minimization '''
+specs = {
+    'edge': {
+        'is_active': 1,
+        'line_tension': 10,
+        'ux': 0.0,
+        'uy': 0.0,
+        'uz': 0.0
+    },
+   'face': {
+       'area_elasticity': 110,
+       'contractility': 0,
+       'is_alive': 1,
+       'prefered_area': 0.5},
+   'settings': {
+       'grad_norm_factor': 1.0,
+       'nrj_norm_factor': 1.0
+   },
+   'vert': {
+       'is_active': 1
+   }
+}
+sheet.vert_df['viscosity'] = 1.0
+# Update the specs (adds / changes the values in the dataframes' columns)
+sheet.update_specs(specs, reset = True)
+geom.update_all(sheet)
+
+# Adjust for cell-boundary adhesion force.
+for i in sheet.edge_df.index:
+    if sheet.edge_df.loc[i, 'opposite'] == -1:
+        sheet.edge_df.loc[i, 'line_tension'] *=2
+    else:
+        continue
+
+for i in sheet.edge_df.index:
+    if sheet.edge_df.loc[i, 'cell_type'] == 'ST':
+        sheet.edge_df.loc[i, 'line_tension'] =0
+    else:
+        continue
+    
+for i in sheet.face_df.index:
+    if sheet.face_df.loc[i,'cell_type'] == 'ST':
+        sheet.face_df.loc[i,'prefered_area'] = 0.3
+    else:
+        continue
+    
+geom.update_all(sheet)
+
+fig, ax = plot_forces(sheet, geom, smodel, ['x', 'y'], scaling=0.1)
+
+
+""" Modelling the CT growth """
+
+from tyssue import History# The History object records all the time steps 
+history = History(sheet)
+
+
+t1_threshold = sheet.edge_df.loc[:,'length'].min() / 10
+t2_threshold = sheet.face_df.loc[:,'area'].min()/10
+division_threshold = sheet.face_df.loc[:,'prefered_area'].min()*0.8
+inhibition_threshold = 0.67
+max_movement = t1_threshold/2
+d_min = t1_threshold
+d_sep = d_min*1.5
+
+t = 0
+t_end = 0.01
+
+while t <= t_end:
+    dt = 0.001
+    print(f'start at t= {round(t, 5)}')
+
+    # Mesh restructure check
+    # T1 transition, edge rearrangment check
+    while True:
+    # Check for any edge below the threshold, starting from index 0 upwards
+        edge_to_process = None
+        for index in sheet.edge_df.index:
+            if sheet.edge_df.loc[index, 'length'] < t1_threshold:
+                edge_to_process = index
+                edge_length = sheet.edge_df.loc[edge_to_process,'length']
+                print(f'Edge {edge_to_process} is too short: {edge_length}')
+                # Process the identified edge with T1 transition
+                type1_transition(sheet, edge_to_process,remove_tri_faces=False, multiplier=1.5)
+                break  
+        # Exit the loop if no edges are below the threshold
+        if edge_to_process is None:
+            break
+    geom.update_all(sheet)
+
+    # T2 transition check.
+    tri_faces = sheet.face_df[(sheet.face_df["num_sides"] < 4) & 
+                          (sheet.face_df["area"] < t2_threshold)].index
+    while len(tri_faces):
+        remove_face(sheet, tri_faces[0])
+        # Recompute the list of triangular faces below the area threshold after each removal
+        tri_faces = sheet.face_df[(sheet.face_df["num_sides"] < 4) & 
+                                  (sheet.face_df["area"] < t2_threshold)].index
+    sheet.reset_index(order = True)
+    geom.update_all(sheet)
+    
+# =============================================================================
+#     
+#     # Cell division.
+#     # Store the centroid before iteration of cells.
+#     unique_edges_df = sheet.edge_df.drop_duplicates(subset='face')
+#     centre_data = unique_edges_df.loc[:,['face','fx','fy']]
+#     # Loop over all the faces.
+#     cells_can_divide = sheet.face_df[(sheet.face_df['area'] >= division_threshold) & (sheet.face_df['T_cycle'] == 0)]
+#     for index, series in cells_can_divide.iterrows():
+#         daughter_index = division_2(sheet,rng=rng, cent_data= centre_data, cell_id = index)
+#     sheet.reset_index(order = True)
+#     geom.update_all(sheet)
+# =============================================================================
+
+    # Force computing and updating positions.
+    valid_active_verts = sheet.active_verts[sheet.active_verts.isin(sheet.vert_df.index)]
+    pos = sheet.vert_df.loc[valid_active_verts, sheet.coords].values
+    # get the movement of position based on dynamical dt.
+    dt, movement = time_step_bot(sheet, dt, max_dist_allowed = max_movement )
+    new_pos = pos + movement
+    # Save the new positions back to `vert_df`
+    sheet.vert_df.loc[valid_active_verts , sheet.coords] = new_pos
+    geom.update_all(sheet)
+    
+    fig,ax = sheet_view(sheet)
+    ax.title.set_text(f't: {t}')
+        
+    t +=dt
+
+
+
 
 
 
