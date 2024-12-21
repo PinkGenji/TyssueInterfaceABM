9f12b3af9b51da7db5c46a1069d23363a3745c22
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2024-12-21 17:11:00 +0800

    slight changes on the parameters

--- Hello Tyssue learning/evolution example.py
+++ Hello Tyssue learning/evolution example.py
@@ -39,14 +39,15 @@ from tyssue.topology.sheet_topology import remove_face, cell_division, face_divi
 
 # 2D plotting
 from tyssue.draw import sheet_view, highlight_cells
+from tyssue.draw.plt_draw import plot_forces
 
 # import my own functions
 from my_headers import *
 
 """ Start programming. """
 # Generate the cell sheet as three cells.
-num_x = 10
-num_y = 2
+num_x = 5
+num_y = 4
 sheet =Sheet.planar_sheet_2d(identifier='bilayer', nx = num_x, ny = num_y, distx = 1, disty = 1)
 geom.update_all(sheet)
 
@@ -82,128 +83,6 @@ sheet.edge_df['cell_type'] = 'to be set'
 for i in list(range(len(sheet.edge_df))):
     sheet.edge_df.loc[i, 'cell_type'] = sheet.face_df.loc[sheet.edge_df.loc[i,'face'],'cell_type']
 
-# First, filter rows where 'cell_type' is 'ST' and 'opposite' is not -1
-rows_to_drop = []
-for i in range(len(sheet.edge_df)):
-    if (sheet.edge_df['cell_type'].iloc[i] == 'ST') and (sheet.edge_df['opposite'].iloc[i] != -1):
-        rows_to_drop.append(i)
-
-# Drop all selected rows at once
-sheet.edge_df.drop(rows_to_drop, inplace=True)
-fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
-
-""" MBSIG demo """
-# First, we need a way to compute the energy, then use gradient descent.
-specs = {
-    'edge': {
-        'is_active': 1,
-        'line_tension': 5,
-        'ux': 0.0,
-        'uy': 0.0,
-        'uz': 0.0
-    },
-   'face': {
-       'area_elasticity': 55,
-       'contractility': 0,
-       'is_alive': 1,
-       'prefered_area': 1},
-   'settings': {
-       'grad_norm_factor': 1.0,
-       'nrj_norm_factor': 1.0
-   },
-   'vert': {
-       'is_active': 1
-   }
-}
-sheet.vert_df['viscosity'] = 1.0
-# Update the specs (adds / changes the values in the dataframes' columns)
-sheet.update_specs(specs, reset = True)
-geom.update_all(sheet)
-
-# Adjust for cell-boundary adhesion force.
-for i in sheet.edge_df.index:
-    if sheet.edge_df.loc[i, 'opposite'] == -1:
-        sheet.edge_df.loc[i, 'line_tension'] *=2
-    else:
-        continue
-geom.update_all(sheet)
-
-# We need set the all the threshold value first.
-t1_threshold = sheet.edge_df.loc[:,'length'].mean()/10
-t2_threshold = sheet.face_df.loc[:,'area'].mean()/10
-division_threshold = sheet.face_df.loc[:,'area'].mean()*2
-growth_speed = sheet.face_df.loc[:,'area'].mean() *0.8
-max_movement = t1_threshold/2
-
-# Now assume we want to go from t = 0 to t= 0.2, dt = 0.1
-t0 = 0
-t_end = 0.05
-dt = 0.01
-time_points = np.linspace(t0, t_end, int((t_end - t0) / dt) + 1)
-print(f'time points are: {time_points}.')
-
-for t in time_points:
-    print(f'start at t= {round(t, 5)}.')
-    
-    # Mesh restructure check
-    # T1 transition, edge rearrangment check
-    while True:
-    # Check for any edge below the threshold, starting from index 0 upwards
-        edge_to_process = None
-        for index in sheet.edge_df.index:
-            if sheet.edge_df.loc[index, 'length'] < t1_threshold:
-                edge_to_process = index
-                edge_length = sheet.edge_df.loc[edge_to_process,'length']
-                print(f'Edge {edge_to_process} is too short: {edge_length}')
-                # Process the identified edge with T1 transition
-                type1_transition(sheet, edge_to_process,remove_tri_faces=False, multiplier=1.5)
-                break  
-        # Exit the loop if no edges are below the threshold
-        if edge_to_process is None:
-            break
-    geom.update_all(sheet)
-
-    # T2 transition check.
-    tri_faces = sheet.face_df[(sheet.face_df["num_sides"] < 4) & 
-                          (sheet.face_df["area"] < t2_threshold)].index
-    while len(tri_faces):
-        remove_face(sheet, tri_faces[0])
-        # Recompute the list of triangular faces below the area threshold after each removal
-        tri_faces = sheet.face_df[(sheet.face_df["num_sides"] < 4) & 
-                                  (sheet.face_df["area"] < t2_threshold)].index
-    sheet.reset_index(order = True)
-    geom.update_all(sheet)
-
-    
-    # Cell division.
-    # Store the centroid before iteration of cells.
-    unique_edges_df = sheet.edge_df.drop_duplicates(subset='face')
-    centre_data = unique_edges_df.loc[:,['face','fx','fy']]
-    # Loop over all the faces.
-    all_cells = sheet.face_df.index
-    for i in all_cells:
-        if sheet.face_df.loc[i,'type'] == 'CT':
-            lateral_split(sheet, cent_data= centre_data, cell_id = i, growth_rate= growth_speed, dt=dt)
-        else:
-            continue
-    sheet.reset_index(order = True)
-    geom.update_all(sheet)
-    
-    # Force computing and updating positions.
-    valid_active_verts = sheet.active_verts[sheet.active_verts.isin(sheet.vert_df.index)]
-    pos = sheet.vert_df.loc[valid_active_verts, sheet.coords].values
-    # Compute the moving direction.
-    dot_r = my_ode(sheet)
-    new_pos = pos + dot_r*dt
-    # Save the new positions back to `vert_df`
-    sheet.vert_df.loc[valid_active_verts , sheet.coords] = new_pos
-    geom.update_all(sheet)
-    
-    # Plot with title contain time.
-    if t in time_points:
-        fig, ax = sheet_view(sheet)
-        ax.title.set_text(f'time = {round(t, 5)}')
-
 
 """ Add another column for division status """
 sheet.face_df['division_status'] = 'ready'
@@ -222,16 +101,16 @@ sheet.face_df['growth_speed'] = 'N/A'
 specs = {
     'edge': {
         'is_active': 1,
-        'line_tension': 0.12,
+        'line_tension': 10,
         'ux': 0.0,
         'uy': 0.0,
         'uz': 0.0
     },
    'face': {
-       'area_elasticity': 1.0,
-       'contractility': 0.04,
+       'area_elasticity': 50,
+       'contractility': 0,
        'is_alive': 1,
-       'prefered_area': 1.0},
+       'prefered_area': 2},
    'settings': {
        'grad_norm_factor': 1.0,
        'nrj_norm_factor': 1.0
@@ -241,7 +120,19 @@ specs = {
    }
 }
 sheet.update_specs(specs, reset = True)
+
+
+# Adjust for cell-boundary adhesion force.
+for i in sheet.edge_df.index:
+    if sheet.edge_df.loc[i, 'opposite'] == -1:
+        sheet.edge_df.loc[i, 'line_tension'] *=2
+    else:
+        continue
 geom.update_all(sheet)
+
+fig, ax = plot_forces(sheet, geom, smodel, ['x', 'y'], scaling=0.1)
+
+
 solver = QSSolver()
 res = solver.find_energy_min(sheet, geom, smodel)
 sheet_view(sheet) 
@@ -269,7 +160,7 @@ history = History(sheet)
 # After spliting, the cells grow to preferred area within 5 time steps.
 
 t = 0
-stop = 10
+stop =10
 
 while manager.current and t < stop:
     for i in sheet.face_df.index:
@@ -277,7 +168,7 @@ while manager.current and t < stop:
         manager.append(lateral_division, cell_id = i, division_rate = 0.03)
         manager.execute(sheet)
         # Find energy min state and record.
-        #res = solver.find_energy_min(sheet, geom, smodel)
+        res = solver.find_energy_min(sheet, geom, smodel)
         history.record()
         # Switch event list from the next list to the current list
         manager.update()
