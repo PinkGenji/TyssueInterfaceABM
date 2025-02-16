272364ab7b048fe2c0e858d591db609ebc067799
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-02-17 10:27:11 +1300

    create post PYR petri dish

---
+++ Hello Tyssue learning/Post PYR petri dish.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+This script simulates a petri dish case after my PhD PYR. 
+In this simulation, the new T3 is implemented.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+This is the end of the script.
+"""
