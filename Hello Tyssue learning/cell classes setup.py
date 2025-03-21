e8ccf38d022a6a9069e9bd549a4cfe69b06d1bbc
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-03-21 16:20:37 +1300

    create cell classes setup.py

---
+++ Hello Tyssue learning/cell classes setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+This script aims to do two major tasks:
+    (1) set up the cell classes we need in the model.
+    (2) Set up a model where there is an initial layer of STB and mature CT,
+    demonstrate that we can swap CT from the mature CT group to the G2 (growing for mitosis)
+    with some probability p.
+"""
+
+
+# =============================================================================
+# First we need to surpress the version warnings from Pandas.
+import warnings 
+warnings.simplefilter(action='ignore', category=FutureWarning) 
+# =============================================================================
+
+# Load all required modules.
+
+import numpy as np
+import pandas as pd
+
+import os
+import json
+import matplotlib as matplot
+import matplotlib.pylab as plt
+import ipyvolume as ipv
+
+from tyssue import Sheet, config #import core object
+from tyssue import PlanarGeometry as geom #for simple 2d geometry
+
+# 2D plotting
+from tyssue.draw import sheet_view, highlight_cells
+from tyssue.draw.plt_draw import plot_forces
+from tyssue.config.draw import sheet_spec
+# import my own functions
+from my_headers import *
+
+rng = np.random.default_rng(70)    # Seed the random number generator.
+
+
+
+
+"""
+This is the end of the script.
+"""
