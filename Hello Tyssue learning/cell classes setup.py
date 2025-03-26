97e6a27e596065f3ceaa965d39b010ce2aed17cf
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-03-26 15:19:50 +1300

    A simple multi cell class setup script

--- Hello Tyssue learning/cell classes setup.py
+++ Hello Tyssue learning/cell classes setup.py
@@ -42,151 +42,49 @@ import my_headers as mh
 rng = np.random.default_rng(70)    # Seed the random number generator.
 
 # Generate the initial cell sheet. Note: 6 horizontal and
-num_x = 6
-num_y = 4
+num_x = rng.integers(10,20)
+num_y = rng.integers(10,20)
 sheet =Sheet.planar_sheet_2d(identifier='bilayer', nx = num_x, ny = num_y, distx = 1, disty = 1)
 geom.update_all(sheet)
 
 # remove non-enclosed faces
 sheet.remove(sheet.get_invalid())
 
-# Delete the irregular polygons, plot the figure to see the index of irregular faces.
-fig, ax = sheet_view(sheet)
-for face, data in sheet.face_df.iterrows():
-    ax.text(data.x, data.y, face)
-plt.show()
-# polygon 4 and 5 are irregular.
-mh.delete_face(sheet, 4)
-mh.delete_face(sheet, 5)
-
+# Delete the irregular polygons.
+for i in sheet.face_df.index:
+    if sheet.face_df.loc[i,'num_sides'] != 6:
+        mh.delete_face(sheet,i)
+    else:
+        continue
 
 sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
 geom.update_all(sheet)
 
-# Plot the figure to see the inital setup is what we want.
+# Plot the figure to see the initial setup is what we want.
 fig, ax = sheet_view(sheet)
 for face, data in sheet.face_df.iterrows():
     ax.text(data.x, data.y, face)
 plt.show()
+print('Initial geometry plot generated. \n')
 
 # Add a new attribute to the face_df, called "cell class"
 sheet.face_df['cell_class'] = 'default'
+total_cell_num = len(sheet.face_df)
+print(f'Attribute "cell class" with value "default" created, there are {total_cell_num} total cells.\n')
+
+"""
+We have a list of all cell classes:
+  STB for resting STB; STB-Ex for extruding stb; S for mature CT, G1 for growing for division CT; 
+M for CT undergoing division; G2 for CT growing for maturity and F for fusing CT.
+"""
+
+print('Now start assigning cell types. \n')
+cell_class_list = ['STB', 'STB-Ex', 'S', 'G1', 'M', 'G2','F']
 for i in sheet.face_df.index:
-    if i in [0,1,2,3,4]:
-        sheet.face_df.loc[i,'cell_class'] = "S" # Set them to be mature CT at start.
-    else:
-        sheet.face_df.loc[i,'cell_class'] = "STB"
-
-
-
-# Now we write a function that for an input of a cell dataframe, it changes
-# 10% of its "S" cells to "G1" class, and doubles the target area of new G1 cells.
-def select_S_to_G1(face_df, rng, percentage=0.1):
-    """Converts 10% of 'S' cells to 'G1' and doubles their target area.
-    
-    Parameters:
-        face_df (DataFrame): DataFrame containing cell properties.
-        rng (np.random.Generator): Random number generator for reproducibility.
-        percentage (float): Probability of conversion (default 10%).
-    """
-
-    s_cells = face_df['cell_class'] == "S"
-    s_indices = face_df.index[s_cells]  # Get indices of the S cells.
-    number_to_change = int(len(s_indices) * percentage)
-    
-    if number_to_change > 0:
-        # Using RNG, we pick the indices of the cells that will change class.
-        selected_indices = rng.choice(s_indices, size=number_to_change, replace=False)
-        face_df.loc[selected_indices, 'cell_class'] = 'G1'  # Update selected cells.
-        face_df.loc[selected_indices, 'prefered_area'] *= 2  # Double the target area.
-
-
-# Now, we need to have a function that converts all "G1" cells that reach 97%
-# of their target area (as an approximation of full size) to be "M" class.
-def select_G1_to_M(face_df):
-    """Converts all 'G1' cells to 'M' when they reach 97% of their target area.
-    
-    Parameters:
-        face_df (DataFrame): DataFrame containing cell properties.
-    """
-
-    for i in face_df.index:
-        if face_df.loc[i, 'class'] == "G1" and face_df.loc[i, 'area'] >= 0.97 * face_df.loc[i, 'prefered_area']:
-            face_df.loc[i, 'class'] = "M" 
-
-
-# Now, we write a function that performs cell division on all "M" cells.
-def divide_M_cells(face_df):
-    """Divides all 'M' cells and assigns both parent and daughter to 'G2'.
-    
-    Parameters:
-        face_df (DataFrame): DataFrame containing cell properties.
-    """
-
-    M_cells = face_df[face_df["class"] == "M"].index.tolist()
-
-    for cell_id in M_cells:
-        # Perform cell division and get the new daughter cell ID
-        daughter_id = cell_division(face_df, cell_id, geom)  
-
-        # Reset properties for both parent and daughter
-        face_df.loc[[cell_id, daughter_id], "prefered_area"] = 1.0
-        face_df.loc[[cell_id, daughter_id], "class"] = "G2"  # Set both to "G2"
-
-        print(f"Cell {cell_id} divided into {daughter_id} and both are now G2")
-
-
-# Now, we write a function that converts all "G2" cells to "S" class when their
-# area is 97% of the target area.
-def select_G2_to_S(face_df):
-    """Converts all 'G2' cells to 'S' when they reach 97% of their target area.
-    
-    Parameters:
-        face_df (DataFrame): DataFrame containing cell properties.
-    """
-
-    for i in face_df.index:
-        if face_df.loc[i, 'class'] == "G2" and face_df.loc[i, 'area'] >= 0.97 * face_df.loc[i, 'prefered_area']:
-            face_df.loc[i, 'class'] = "S" 
-
-
-# Now, we write a function that converts an "S" cell to an "F" cell with 10%
-# probability, if it has at least one neighboring "STB" cell.
-def select_S_to_F(face_df, sheet, rng, percentage=0.1):
-    """Converts 10% of 'S' cells to 'F' if they have an 'STB' neighbor.
-    
-    Parameters:
-        face_df (DataFrame): DataFrame containing cell properties.
-        sheet: The tissue sheet object (to get neighbors).
-        rng (np.random.Generator): Random number generator for reproducibility.
-        percentage (float): Probability of conversion (default 10%).
-    """
-
-    s_cells = face_df["class"] == "S"
-    s_indices = face_df.index[s_cells]  # Get indices of the S cells
-
-    # Filter only those with at least one 'STB' neighbor
-    valid_indices = [i for i in s_indices if any(face_df.loc[n, "class"] == "STB" for n in sheet.get_neighbours(i))]
-
-    number_to_change = int(len(valid_indices) * percentage)
-    
-    if number_to_change > 0:
-        # Randomly select which 'S' cells will turn into 'F'
-        selected_indices = rng.choice(valid_indices, size=number_to_change, replace=False)
-        face_df.loc[selected_indices, "class"] = "F"  # Update selected cells
-
-
-
-
-
-
-
-
-
-
-
-
-
+    sheet.face_df.loc[i,'cell_class'] = rng.choice(cell_class_list)     # randomly select one cell class for the cell.
+cell_class_table = sheet.face_df['cell_class'].value_counts()
+print('The table summaries the number of different class of cells we have: ')
+print(cell_class_table)
 
 
 
