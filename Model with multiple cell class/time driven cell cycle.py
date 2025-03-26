97d4e4729bc34350946759c0348b8331417400c8
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-03-27 10:15:34 +1300

    created time driven cell cycle.py

---
+++ Model with multiple cell class/time driven cell cycle.py
@@ -0,0 +1,21 @@
+"""
+This script models a time driven (and only time) cell cycle. Following are the aims:
+    (1) Mature CTs Cell gets recruited from mature into G2 with simple probability p
+    (2) Cell does not grow but is programmed to stay in G2 for a fixed period of time
+    (3) Cell moves to M phase and divides
+    (4) Two daughter cells now in G1 don’t actively grow, but instead are programmed to stay in G1 for fixed amount of time
+    (5) Cells move from G1 to M
+    Note: the only 'cell growth' factor is the target area term in the energy function.
+"""
+
+
+
+
+
+
+
+
+
+
+
+print('\n This is the end of this script. (＾• ω •＾)')
