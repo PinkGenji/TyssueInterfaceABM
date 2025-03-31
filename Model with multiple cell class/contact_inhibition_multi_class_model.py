251dbdf82c38c7a77b12488d0ea5e2ab03bee34e
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-04-01 11:24:22 +1300

    created contact inhibition multi class model.py

---
+++ Model with multiple cell class/contact_inhibition_multi_class_model.py
@@ -0,0 +1,8 @@
+"""
+This script is improved from the contact inhibition model with single cell class by implement multi cell classes and
+the transitions between different classes.
+"""
+
+
+
+print('\n This is the end of this script. (＾• ω •＾) ')
