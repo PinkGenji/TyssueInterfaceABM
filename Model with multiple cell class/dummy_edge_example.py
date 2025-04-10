97f6471e5063b190b9691c535a08ba37416c96cc
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-04-10 13:35:27 +1200

    created dummy edge example script

---
+++ Model with multiple cell class/dummy_edge_example.py
@@ -0,0 +1,12 @@
+"""
+This script produces a bilayer geometry first, then converts all edges between STB units into dummy edges.
+A video of evolution is created in the end.
+"""
+
+
+
+
+
+
+
+print('\n This is the end of this script. (＾• ω •＾) ')
