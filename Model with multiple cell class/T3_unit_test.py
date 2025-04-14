e9ff388a041d07a235a6d1a1a47b98c197e5ef1f
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-04-14 15:55:30 +1200

    adjusted case 3 vertices
    checked using geogebra, distance (0.067) between vert 12 and edge 104 is within the distance of d_min (0.13)

--- Model with multiple cell class/T3_unit_test.py
+++ Model with multiple cell class/T3_unit_test.py
@@ -4,13 +4,6 @@
 This script is a unit test for T3 function.
 """
 
-
-# =============================================================================
-# First we need to surpress the version warnings from Pandas.
-import warnings 
-warnings.simplefilter(action='ignore', category=FutureWarning) 
-# =============================================================================
-
 # Load all required modules.
 
 import numpy as np
@@ -23,21 +16,7 @@ import matplotlib.pylab as plt
 import ipyvolume as ipv
 
 from tyssue import Sheet, config #import core object
-from tyssue import PlanarGeometry as geom #for simple 2d geometry
 
-# For cell topology/configuration
-from tyssue.topology.sheet_topology import type1_transition
-from tyssue.topology.base_topology import collapse_edge, remove_face, add_vert, merge_vertices
-from tyssue.topology.sheet_topology import split_vert as sheet_split
-from tyssue.topology.bulk_topology import split_vert as bulk_split
-from tyssue.topology import condition_4i, condition_4ii
-
-## model and solver
-from tyssue.dynamics.planar_vertex_model import PlanarModel as smodel
-from tyssue.solvers.quasistatic import QSSolver
-from tyssue.generation import extrude
-from tyssue.dynamics import model_factory, effectors
-from tyssue.topology.sheet_topology import remove_face, cell_division, face_division
 
 # 2D plotting
 from tyssue.draw import sheet_view, highlight_cells
@@ -48,10 +27,10 @@ from my_headers import *
 
 from T3_function import *
 
-""" Start programming. """
+""" Start testing. """
 # Generate the cell sheet as three cells.
-num_x = 5
-num_y = 4
+num_x = 7
+num_y = 6
 sheet =Sheet.planar_sheet_2d(identifier='bilayer', nx = num_x, ny = num_y, distx = 1, disty = 1)
 geom.update_all(sheet)
 
@@ -62,7 +41,7 @@ sheet.remove(sheet.get_invalid())
 fig, ax = sheet_view(sheet)
 for face, data in sheet.face_df.iterrows():
     ax.text(data.x, data.y, face)
-    
+plt.show()
 delete_face(sheet, 5)
 delete_face(sheet, 6)
 delete_face(sheet, 17)
@@ -71,21 +50,17 @@ delete_face(sheet,18)
 sheet.reset_index(order=True)   #continuous indices in all df, vertices clockwise
 geom.update_all(sheet)
 sheet.get_extra_indices()   # extra_indices are not included in update_all.
-# Setup d_min and d_sep values.
-d_min = sheet.edge_df.loc[:,'length'].min()/10
-d_sep = d_min*3
-
-print(f'd_min is set: {d_min}, d_sep is set: {d_sep}')
-
-# Plot figures to check.
-# Draw the cell mesh with face labelling and edge arrows.
-fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
-for face, data in sheet.vert_df.iterrows():
-    ax.text(data.x, data.y, face)
-
 fig, ax = sheet_view(sheet)
 for face, data in sheet.face_df.iterrows():
     ax.text(data.x, data.y, face)
+plt.show()
+
+# Setup d_min and d_sep values.
+d_min = sheet.edge_df.loc[:,'length'].min()/10
+d_sep = d_min*1.5
+
+print(f'd_min is set: {d_min}, d_sep is set: {d_sep}')
+
 
 """ Make adjustments to the geometry for unit test.  
 Note: the code for case 1 and case 3 were wrote first, since creating case 2 
@@ -94,8 +69,8 @@ and case 4 involves vertex reindex. Hence I wrote case 1 & 3 first.
 
 # Case 1 from Fletcher 2013, move vertex number 13 close enough to 
 # edge number 57, which is connecting vertex 51 and 50.
-sheet.vert_df.loc[13,'x'] = 5.7
-sheet.vert_df.loc[13,'y'] = 2.55
+sheet.vert_df.loc[13,'x'] = 5.5
+sheet.vert_df.loc[13,'y'] = 2.6
 
 geom.update_all(sheet)
 sheet.get_extra_indices()  
@@ -103,8 +78,9 @@ sheet.get_extra_indices()
 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
 for face, data in sheet.vert_df.iterrows():
     ax.text(data.x, data.y, face)
+plt.show()
 
-
+print('Starts T3 on case 1.')
 while True:
     T3_todo = None
     print('computing boundary indices.')
@@ -128,6 +104,7 @@ while True:
                 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
                 for face, data in sheet.vert_df.iterrows():
                     ax.text(data.x, data.y, face)
+                plt.show()
                 break
         
         if T3_todo is not None:
@@ -150,7 +127,7 @@ d_sep = d_min*3
 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
 for face, data in sheet.vert_df.iterrows():
     ax.text(data.x, data.y, face)
-
+plt.show()
 
 
 # =============================================================================
@@ -176,7 +153,7 @@ for face, data in sheet.vert_df.iterrows():
 # =============================================================================
 
 
-''' Do T3 '''
+print('Starts T3 on case 3.')
 while True:
     T3_todo = None
     print('computing boundary indices.')
@@ -200,6 +177,7 @@ while True:
                 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
                 for face, data in sheet.vert_df.iterrows():
                     ax.text(data.x, data.y, face)
+                plt.show()
                 break
         
         if T3_todo is not None:
@@ -222,16 +200,19 @@ sheet.reset_index()
 geom.update_all(sheet)
 
 sheet.vert_df.loc[41,'y'] = 5
-sheet.vert_df.loc[12,'x'] = 3.3
-sheet.vert_df.loc[12,'y'] = 4.8
 sheet.vert_df.loc[10,'y'] =3.8
+sheet.vert_df.loc[12,'x'] = 3.3
+sheet.vert_df.loc[12,'y'] = 4.6
 
 geom.update_all(sheet)
+print(dist_computer(sheet,104,12,d_sep))
 sheet.get_extra_indices()
 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
 for face, data in sheet.vert_df.iterrows():
     ax.text(data.x, data.y, face)
+plt.show()
 
+print('Starts T3 on case 4.')
 while True:
     T3_todo = None
     print('computing boundary indices.')
@@ -254,6 +235,7 @@ while True:
                 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
                 for face, data in sheet.vert_df.iterrows():
                     ax.text(data.x, data.y, face)
+                plt.show()
                 break
         
         if T3_todo is not None:
@@ -276,7 +258,9 @@ sheet.get_extra_indices()
 fig, ax = sheet_view(sheet, edge = {'head_width':0.1})
 for face, data in sheet.vert_df.iterrows():
     ax.text(data.x, data.y, face)
-    
+plt.show()
+
+print('start T3 on case 2.')
 while True:
     T3_todo = None
     print('computing boundary indices.')
