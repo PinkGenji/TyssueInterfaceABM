1bc4ed20cf85392a98cc5b432519d521e4bd6d26
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-06-03 11:43:20 +1200

    created STB_removal_101.py

---
+++ Model with multiple cell class/STB_removal_101.py
@@ -0,0 +1,9 @@
+"""
+This script creates a trilayer geometry, then artificially detach an STB unit from the CTs.
+The goal of this script is to investigate how we should remove an STB unit from its layer.
+"""
+
+
+
+
+print('\n This is the end of this script. (＾• ω •＾) ')
