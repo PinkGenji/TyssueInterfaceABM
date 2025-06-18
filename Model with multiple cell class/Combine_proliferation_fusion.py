1c481e42c4533fb378f7938586b94caa7b7a6b11
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-06-18 15:58:03 +1200

    created: combine proliferation and fusion script

---
+++ Model with multiple cell class/Combine_proliferation_fusion.py
@@ -0,0 +1,12 @@
+"""
+This script contains a vertex model that starts with a bilayer structure.
+The system has a multi-class system. Two cellular behaviours are modelled: cell proliferation and cell fusion.
+"""
+
+
+
+
+
+
+
+print('\n This is the end of this script. (＾• ω •＾) ')
