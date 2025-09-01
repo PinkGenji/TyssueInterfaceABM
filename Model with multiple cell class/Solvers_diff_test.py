ac19422b71288538e761a6cff2ce8bb745f45727
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2025-09-01 14:30:34 +1200

    added switch for using different thresholds

--- Model with multiple cell class/Solvers_diff_test.py
+++ Model with multiple cell class/Solvers_diff_test.py
@@ -5,28 +5,25 @@ This script works as a lab bench for
 # Load all required modules.
 import random
 import numpy as np
+import os
 import re
 import matplotlib.pyplot as plt
 from tyssue import Sheet, History, PlanarGeometry
 from tyssue.topology.sheet_topology import remove_face
 from tyssue.dynamics import effectors, model_factory
 from tyssue.behaviors import EventManager
-from tyssue.behaviors.sheet import basic_events
 from tyssue.solvers.viscous import EulerSolver
-from tyssue.io import hdf5 # For saving the datasets
+
+
 
 # 2D plotting
 from tyssue.draw import sheet_view
-from tyssue.draw.plt_draw import create_gif
-from IPython.display import Image, display
-from tyssue.topology.sheet_topology import cell_division
+from tyssue.config.draw import sheet_spec
 
 # import my own functions
 from my_headers import *
 from T3_function import *
 
-import os
-import imageio.v2 as imageio
 
 
 # Moving function definitions to the top of the file
@@ -35,7 +32,7 @@ def drop_face(sheet, face, **kwargs):
     Removes the face indexed by "face" and all associated edges
     """
     edge = sheet.edge_df.loc[(sheet.edge_df['face'] == face)].index
-    print(f"Removing face '{face}'")
+    print(f"Dropping face '{face}'")
     sheet.remove(edge, **kwargs)
 
 
@@ -101,27 +98,10 @@ def cell_cycle_transition(sheet, manager, dt, cell_id=0, p_recruit=0.1, G2_durat
         manager.append(cell_cycle_transition, dt=dt, cell_id=cell_id)
 
 
-# Helper function to extract the numeric part from a filename for later use.
-# For example, from "frame_12.png", it extracts 12
-def extract_number(fname):
-    match = re.search(r'\d+', fname)
-    return int(match.group()) if match else -1  # If no number found, use -1
-
-
-# Define the directory name
-frames_dir = "frames"
-# Create directory for frames
-if not os.path.exists(frames_dir):
-    print(f"Directory '{frames_dir}' does not exist. Creating it.")
-    os.makedirs(frames_dir)
-else:
-    print(f"Directory '{frames_dir}' already exists. Using existing folder.")
-
 random.seed(42)  # Controls Python's random module (e.g. event shuffling)
 np.random.seed(42)  # Controls NumPy's RNG (e.g. vertex positions, topology)
 
 rng = np.random.default_rng(70)  # Seed the random number generator for my own division function.
-
 
 # Generate the initial cell sheet for bilayer.
 geom = PlanarGeometry
@@ -141,22 +121,47 @@ sheet.remove(sheet.get_invalid())
 while np.any(sheet.face_df['num_sides'].values != 6):
     bad_face = sheet.face_df[sheet.face_df['num_sides'] != 6].index[0]
     drop_face(sheet, bad_face)
-
+# Update the geometry and computes the grabs all pairs of half-edges.
 geom.update_all(sheet)
 sheet.get_opposite()
 
+# Add a new attribute to the face_df, called "cell class"
+sheet.face_df['cell_class'] = 'default'
+sheet.face_df['timer'] = 'NA'
+total_cell_num = len(sheet.face_df)
+print('New attributes: cell_class; timer created for all cells. \n ')
+
+for i in range(0, num_x - 2):  # These are the indices of the bottom layer.
+    sheet.face_df.loc[i, 'cell_class'] = 'G1'
+    # Add a timer for each cell enters "G1".
+    sheet.face_df.loc[i, 'timer'] = 0.11
+
+for i in range(num_x - 2, len(sheet.face_df)):  # These are the indices of the top layer.
+    sheet.face_df.loc[i, 'cell_class'] = 'STB'
+
+# Apply drawing specs, so STB and CT have different colours.
+draw_specs = sheet_spec()
+# Enable face visibility.
+draw_specs['face']['visible'] = True
+for i in sheet.face_df.index:  # Assign face colour based on the cell class.
+    if sheet.face_df.loc[i, 'cell_class'] == 'STB':
+        sheet.face_df.loc[i, 'color'] = 0.7
+    else:
+        sheet.face_df.loc[i, 'color'] = 0.1
+draw_specs['face']['color'] = sheet.face_df['color']
+draw_specs['face']['alpha'] = 0.2  # Set transparency.
+
 # Plot the figure to see the initial setup is what we want.
-fig, ax = sheet_view(sheet)
-for face, data in sheet.face_df.iterrows():
-    ax.text(data.x, data.y, face)
+fig, ax = sheet_view(sheet, **draw_specs)
 plt.show()
 ax.set_title("Initial Bilayer Setup")  # Adding title
-
 print('Initial geometry plot generated. \n')
+print(f'There are {total_cell_num} total cells; equally split into "G1" and "STB" classes. \n')
 
+# Setup what effectors we want for the model
 model = model_factory([effectors.LineTension, effectors.FaceContractility, effectors.FaceAreaElasticity])
 
-# Add dynamics to the model.
+# specify the values of parameters to the model.
 specs = {
     'edge': {
         'is_active': 1,
@@ -179,7 +184,7 @@ specs = {
     }
 }
 sheet.vert_df['viscosity'] = 1.0
-# Update the specs (adds / changes the values in the dataframes' columns)
+# Update the specs to the dataframes.
 sheet.update_specs(specs, reset=True)
 sheet.get_opposite()
 geom.update_all(sheet)
@@ -192,22 +197,6 @@ for i in sheet.edge_df.index:
         continue
 geom.update_all(sheet)
 
-# Add a new attribute to the face_df, called "cell class"
-sheet.face_df['cell_class'] = 'default'
-sheet.face_df['timer'] = 'NA'
-total_cell_num = len(sheet.face_df)
-print('New attributes: cell_class; timer created for all cells. \n ')
-
-for i in range(0, num_x - 2):  # These are the indices of the bottom layer.
-    sheet.face_df.loc[i, 'cell_class'] = 'G1'
-    # Add a timer for each cell enters "G1".
-    sheet.face_df.loc[i, 'timer'] = 0.11
-
-for i in range(num_x - 2, len(sheet.face_df)):  # These are the indices of the top layer.
-    sheet.face_df.loc[i, 'cell_class'] = 'STB'
-
-print(f'There are {total_cell_num} total cells; equally split into "G1" and "STB" classes. ')
-
 # Set the value of constants for mesh restructure, which are parts of my own solver in loop.
 t1_threshold = sheet.edge_df.length.mean() / 10
 t2_threshold = sheet.face_df['area'].mean() / 10
@@ -215,30 +204,15 @@ d_min = t1_threshold
 d_sep = d_min * 1.5
 max_movement = t1_threshold *(2**0.5)
 
-# Apply drawing specs, so STB and CT have different colours.
-from tyssue.config.draw import sheet_spec
-
-draw_specs = sheet_spec()
-# Enable face visibility.
-draw_specs['face']['visible'] = True
-for i in sheet.face_df.index:  # Assign face colour based on the cell class.
-    if sheet.face_df.loc[i, 'cell_class'] == 'STB':
-        sheet.face_df.loc[i, 'color'] = 0.7
-    else:
-        sheet.face_df.loc[i, 'color'] = 0.1
-draw_specs['face']['color'] = sheet.face_df['color']
-draw_specs['face']['alpha'] = 0.2  # Set transparency.
-
 # Start simulating.
 # Initialise the Event Manager for Tyssue's Built-in Euler Solver.
 manager = EventManager('face')
 history = History(sheet)  # Record the initial configuration of the sheet.
-t = 0
-t_end = 0.0006
 
-while t <= t_end:
+i = 0
+# Only one iteration
+while i < 1:
     dt = 0.0005
-    print(f"Evaluating time: {t}")
     # Mesh restructure check
     # T1 transition, edge rearrangment check
     while True:
@@ -316,7 +290,6 @@ while t <= t_end:
             sheet.face_df.loc[cell, 'timer'] = 0.4
         else:
             continue
-
     geom.update_all(sheet)
 
     # At the end of the timer, "G2" becomes "M".
@@ -326,7 +299,6 @@ while t <= t_end:
             sheet.face_df.loc[cell, 'cell_class'] = 'M'
         else:
             sheet.face_df.loc[cell, 'timer'] -= dt
-
     geom.update_all(sheet)
 
     # Cell division.
@@ -343,7 +315,6 @@ while t <= t_end:
         # Add a timer for each cell enters "G1".
         sheet.face_df.loc[index, 'timer'] = 0.11
         sheet.face_df.loc[daughter_index, 'timer'] = 0.11
-
     geom.update_all(sheet)
 
     # At the end of the timer, "G1" class becomes "S".
@@ -351,8 +322,6 @@ while t <= t_end:
     for cell in G1_cells:
         if sheet.face_df.loc[cell, 'timer'] < 0:
             sheet.face_df.loc[cell, 'cell_class'] = 'S'
-            if cell == 1:
-                print(f'Cell 1 enter "S" at time {t}. ')
         else:
             sheet.face_df.loc[cell, 'timer'] -= dt
 
@@ -362,37 +331,43 @@ while t <= t_end:
     # Force computing and updating positions using my own solver.
     valid_active_verts = sheet.active_verts[sheet.active_verts.isin(sheet.vert_df.index)]
     pos = sheet.vert_df.loc[valid_active_verts, sheet.coords].values.ravel()
-    # get the movement of position based on dynamical dt.
-    dt, movement = time_step_bot(sheet, dt, max_dist_allowed=max_movement*dt)
+    # Switch for different threshold of different solvers.
+    different_threshold = True
+    if different_threshold:
+        max_dist = t1_threshold * (2**0.5) * dt
+    else:
+        max_dist = t1_threshold
+    dt, movement = time_step_bot(sheet, dt, max_dist_allowed=max_dist )
     new_pos = pos + movement
     # Save the new positions back to `vert_df`
     sheet.vert_df.loc[valid_active_verts, sheet.coords] = new_pos.reshape((-1, sheet.dim))
     geom.update_all(sheet)
     history.record()
-    fig, ax = sheet_view(sheet)
+    fig, ax = sheet_view(sheet, **draw_specs)
     ax.set_title('After a single step with MyOwnSolver')
     plt.show()
-    # Write the final sheet to a hdf5 file.
-    hdf5.save_datasets('SingleStepMyOwnSolver.hdf5', sheet)
 
-
-    # Reinitialise the geometry then use debug mode in viscous.py
+    # Revert the geometry to the initial state, then use Tyssue's built-in Euler Solver.
     sheet = history.retrieve(0)
     history.record()
-    fig, ax = sheet_view(sheet)
+    # See if the revert was successful, pay attention to the size of polygons.
+    fig, ax = sheet_view(sheet, **draw_specs)
     ax.set_title('Retrieved initial geometry')
     plt.show()
+    # Initialise the Tyssue's Euler solver.
     solver = EulerSolver(sheet, geom, model, manager=manager, bounds=(-t1_threshold, t1_threshold))
-    movement2, dot_r2 = solver.solve(tf=dt, dt=dt)
-    # now, compare movement and movement2
-    movement = movement.ravel()
-
+    # Grab the movement and dot_r computed from Tyssue's Euler solver.
+    movement2, dot_r2 = solver.single_step_movement(tf=dt, dt=dt)
+    # Now, compare movement and movement2
     tolerance = 1e-4
     all_close_enough = np.all(np.abs(movement - movement2) <= tolerance)
-    print(f'Comparing values between two groups, are all values differ within the tolerance level ({tolerance})? {all_close_enough}')
+    # Print related values to the console.
+    print(f'After all the adjustment, dt is now: {dt}')
+    print(f'Comparing values between two groups...\n Is it true all values only differ within the tolerance level ({tolerance})? \n'
+          f' {all_close_enough}')
     biggest_gap = max((abs(movement-movement2)))
     print(f'The biggest gap between values from movement and movement2 is {biggest_gap}')
 
-    t += dt
+    i += 1  # Finish the iteration
 
 print('\n This is the end of this script. (＾• ω •＾) ')
