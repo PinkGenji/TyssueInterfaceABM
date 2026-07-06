9e95d2638bbc2e0b5137df83faa6b5340d824ec7
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2026-07-07 10:52:30 +1200

    fusion probability is 20%

--- stochastic_duration_model/stochastic_duration_model.py
+++ stochastic_duration_model/stochastic_duration_model.py
@@ -302,7 +302,7 @@ def stb_extrusion(sheet, cell_id):
 
 
 # Define the directory name
-frames_dir = "frames_stochastic_0"
+frames_dir = "frames_stochastic_20"
 # Create directory for frames
 if not os.path.exists(frames_dir):
     print(f"Directory '{frames_dir}' does not exist. Creating it.")
@@ -582,26 +582,26 @@ while t <= t_end:
         if sheet.face_df.loc[cell, 'timer'] >0:
             sheet.face_df.loc[cell, 'timer'] -= dt
             # Following part is commented out to disable fusion happen, which is 0 percent of fusion.
-        # else:
-        #     can_fuse = 0
-        #     neighbours = sheet.get_neighbors(cell)
-        #     for i in neighbours:
-        #         if sheet.face_df.loc[i, 'cell_class'] == 'STB':
-        #             can_fuse = 1
-        #         else:
-        #             continue
-        #     # Use rng to randomly generate a number between 1 and 10, this will determine the fate of the mature CT.
-        #     cell_fate_roulette = rng.random()
-        #     if can_fuse == 1 and cell_fate_roulette < 0.05:  # If CT is adjacent to STB, then it has 5% probability to fuse.
-        #         sheet.face_df.loc[cell, 'cell_class'] = 'F'
-        #         # Add a timer for each cell enters 'F'.
-        #         sheet.face_df.loc[cell, 'timer'] = round(rng.uniform(tau_F_min, tau_F_max), 4)
-        #         fusion_count += 1
         else:
-            sheet.face_df.loc[cell, 'cell_class'] = 'G2'
-            # Add a timer for each cell enters "G2".
-            sheet.face_df.loc[cell, 'timer'] = round(rng.uniform(tau_G2_min, tau_G2_max), 4)
-    geom.update_all(sheet)
+            can_fuse = 0
+            neighbours = sheet.get_neighbors(cell)
+            for i in neighbours:
+                if sheet.face_df.loc[i, 'cell_class'] == 'STB':
+                    can_fuse = 1
+                else:
+                    continue
+            # Use rng to randomly generate a number between 1 and 10, this will determine the fate of the mature CT.
+            cell_fate_roulette = rng.random()
+            if can_fuse == 1 and cell_fate_roulette < 0.2:  # If CT is adjacent to STB, then it has 20% probability to fuse.
+                sheet.face_df.loc[cell, 'cell_class'] = 'F'
+                # Add a timer for each cell enters 'F'.
+                sheet.face_df.loc[cell, 'timer'] = round(rng.uniform(tau_F_min, tau_F_max), 4)
+                fusion_count += 1
+            else:
+                sheet.face_df.loc[cell, 'cell_class'] = 'G2'
+                # Add a timer for each cell enters "G2".
+                sheet.face_df.loc[cell, 'timer'] = round(rng.uniform(tau_G2_min, tau_G2_max), 4)
+        geom.update_all(sheet)
 
     # At the end of the timer, "G2" becomes "M".
     G2_cells = sheet.face_df.index[sheet.face_df['cell_class'] == 'G2'].tolist()
@@ -645,18 +645,17 @@ while t <= t_end:
     sheet.reset_index(order=True)
     geom.update_all(sheet)
 
-    # Since fusion is disabled, we no longer have an 'F' class.
-    # # At the end of a timer, "F" class becomes "STB" and dummy edge is generated.
-    # F_cells = sheet.face_df.index[sheet.face_df['cell_class'] == 'F'].tolist()
-    # for cell in F_cells:
-    #     if sheet.face_df.loc[cell, 'timer'] < 0:
-    #         fusing_cell = fuse_single_cell(sheet, cell, tau_F_min, tau_F_max)
-    #         fusing_cell_idx = sheet.face_df[sheet.face_df['unique_id'] == fusing_cell].index
-    #         sheet.face_df.loc[fusing_cell_idx, 'cell_class'] = 'STB'
-    #         sheet.face_df.loc[fusing_cell_idx,'timer'] = np.nan # As a fresh STB unit, reset the timer to nan
-    #     else:
-    #         sheet.face_df.loc[cell, 'timer'] -= dt
-    # geom.update_all(sheet)
+    # At the end of a timer, "F" class becomes "STB" and dummy edge is generated.
+    F_cells = sheet.face_df.index[sheet.face_df['cell_class'] == 'F'].tolist()
+    for cell in F_cells:
+        if sheet.face_df.loc[cell, 'timer'] < 0:
+            fusing_cell = fuse_single_cell(sheet, cell, tau_F_min, tau_F_max)
+            fusing_cell_idx = sheet.face_df[sheet.face_df['unique_id'] == fusing_cell].index
+            sheet.face_df.loc[fusing_cell_idx, 'cell_class'] = 'STB'
+            sheet.face_df.loc[fusing_cell_idx,'timer'] = np.nan # As a fresh STB unit, reset the timer to nan
+        else:
+            sheet.face_df.loc[cell, 'timer'] -= dt
+    geom.update_all(sheet)
 
     # # Extrude the 'E' units before assigning new 'E' units.
     # E_units = sheet.face_df.index[sheet.face_df['cell_class'] == 'E'].tolist()
@@ -705,7 +704,7 @@ while t <= t_end:
     ax.title.set_text(f'time = {real_time_hours}')
     ax.set_axis_off()
     # Save to file instead of showing.
-    frame_path = f"frames_stochastic_0/frame_{real_time_hours}.png"
+    frame_path = f"frames_stochastic_20/frame_{real_time_hours}.png"
     plt.savefig(frame_path)
     plt.close(fig)  # Close figure to prevent memory leaks
 
@@ -717,11 +716,11 @@ final_stb_ct_interface_length = stb_ct_interface_length(sheet)
 final_stb_thickness = final_stb_area/final_stb_ct_interface_length
 
 # Write the final sheet to a hdf5 file.
-hdf5.save_datasets('stochastic_0_percent.hdf5', sheet)
+hdf5.save_datasets('stochastic_20_percent.hdf5', sheet)
 
 """ Generate the video based on the frames saved. """
 # Path to folder containing the frame images
-frame_folder = "frames_stochastic_0"
+frame_folder = "frames_stochastic_20"
 
 # Helper function to extract the numeric part from a filename
 # For example, from "frame_12.png", it extracts 12
@@ -737,7 +736,7 @@ frame_files = sorted([
 ], key=lambda x: extract_number(os.path.basename(x)))  # Sort by extracted number
 
 # Create a video with 15 frames per second, change the name to whatever you want the name of mp4 to be.
-with imageio.get_writer('stochastic_0_percent.mp4', fps=15, format='ffmpeg') as writer:
+with imageio.get_writer('stochastic_20_percent.mp4', fps=15, format='ffmpeg') as writer:
     # Read and append each frame in sorted order
     for filename in frame_files:
         image = imageio.imread(filename)  # Load image from the folder
@@ -785,7 +784,7 @@ df = pd.DataFrame({
     "STB_area": STB_area
 })
 # Save to CSV
-df.to_csv("stochastic_0_percent.csv", index=False)
+df.to_csv("stochastic_20_percent.csv", index=False)
 print("Saved simulation output csv")
 
 print(f' The initial STB area is {initial_stb_area:.2f},\n the initial STB-CT interface length is {initial_stb_ct_interface_length:.2f},\n and the initial mean thickness is {initial_stb_thickness:.2f}.\n')
